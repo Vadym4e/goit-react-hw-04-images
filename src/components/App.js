7f1105a546e1cef7874c82969f2c2d5c93fdbab7
Vadym4e <vadym.o92@gmail.com>
2023-09-09 21:35:07 +0300

    loader

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import SearchBar from './SearchBar/SearchBar';
 import Gallery from './Gallery/Gallery';
 import LoadMore from './LoadMore/LoadMore';
 import { fetchStartImages, fetchRequestImages } from 'api';
+import { Loader } from './Loader/Loader';
+
 const { Component } = require('react');
 
 export class App extends Component {
@@ -10,6 +12,8 @@ export class App extends Component {
     query: '',
     images: [],
     page: 1,
+    total: 0,
+    isLoading: false,
   };
 
   changeQuery = newQuery => {
@@ -20,13 +24,32 @@ export class App extends Component {
     this.setState({ query: `${Date.now()}/${newQuery}`, images: [], page: 1 });
   };
 
+  LoadImages = async () => {
+    this.setState({ isLoading: true });
+
+    const normalQuery = this.state.query.slice(14, this.state.query.length);
+
+    await fetchRequestImages(normalQuery, this.state.page)
+      .then(images => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...images.hits],
+          total: images.total,
+          isLoading: false,
+        }));
+      })
+      .catch(error => {
+        console.error('Error fetching images:', error);
+        this.setState({ isLoading: false });
+      });
+  };
+
   async componentDidMount() {
     const imagesList = await fetchStartImages();
 
-    this.setState(({ images }) => ({
+    this.setState({
       images: [...imagesList.hits],
-    }));
-    // console.log(imagesList);
+      total: imagesList.total,
+    });
   }
 
   async componentDidUpdate(prevProps, prevState) {
@@ -34,15 +57,7 @@ export class App extends Component {
       prevState.query !== this.state.query ||
       prevState.page !== this.state.page
     ) {
-      const normalQuery = this.state.query.slice(14, this.state.query.length);
-
-      const imagesList = await fetchRequestImages(normalQuery, this.state.page);
-
-      this.setState(({ images }) => ({
-        images: [...images, ...imagesList.hits],
-      }));
-      console.log(normalQuery);
-      console.log(imagesList);
+      this.LoadImages();
     }
   }
 
@@ -51,11 +66,15 @@ export class App extends Component {
   };
 
   render() {
+    const { images, isLoading, total } = this.state;
     return (
       <div>
         <SearchBar search={this.changeQuery} />
-        <Gallery images={this.state.images} />
-        <LoadMore moreImages={this.loadMoreImages} />
+        <Loader />
+        <Gallery images={images} />
+        {images.length < total && !isLoading && (
+          <LoadMore moreImages={this.loadMoreImages} />
+        )}
       </div>
     );
   }
